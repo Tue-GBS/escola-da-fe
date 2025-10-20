8d5709cdfabb0ab615170f63e16878249e4a7e7b
Tue-GBS <mateusdasilva72@outlook.com>
2025-10-20 10:08:23 -0300

    V2.7 - incluindo PHP

--- head.php
+++ head.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu Leve</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/card_adms.css">
-    <link rel="stylesheet" href="css/card.css">
-</head>
-<body>
     <nav class="menu">
         <!-- Item do Menu Principal -->
         <div class="menu-item has-submenu">
@@ -75,42 +64,3 @@
             </button>
         </div>
     </nav>
-
-    <main>
-        <section>
-          <div class="cards-container">
-            <div class="card">
-              <img class="card-img" src="/assets/img/banner.png" alt="banner">
-              <div class="card-content">
-                <h3 class="card-title">Bem-vindo(a)! à Escola da Fé</h3>
-                <p class="card-text">Seu canal de estudos bíblicos</p>
-              </div>
-            </div>
-            <div class="card">
-              <img class="card-img" src="/assets/img/img-padre.jpg" alt="Pe. Francisco Valbério Soares Bruno">
-              <div class="card-content">
-                <h3 class="card-title">Pe. Francisco Valbério Soares Bruno</h3>
-                <p class="card-text">Administrador Paroquial</p>
-              </div>
-            </div>
-            <div class="card">
-              <img class="card-img" src="/assets/img/dom francisco.png" alt="Dom. Francisco Agamenilton Damascena">
-              <div class="card-content">
-                <h3 class="card-title">Dom. Francisco Agamenilton Damascena</h3>
-                <p class="card-text">Bispo Diocesano de Luziânia</p>
-              </div>
-            </div>
-          </div>
-        </section>
-    </main>
-
-    <script src="js/script.js"></script>
-    <script src="https://www.youtube.com/iframe_api"></script>
-
-    <footer>
-        <p>Atenciosamente,</p>
-        <p>Mateus Gonçalves Batista da Silva</p>
-        <p>Técnico de Informática | Estudante de ADS</p>
-    </footer>
-</body>
-</html>
---
+++ sobre.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Leve</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="js/script.js">
+</head>
+<body>
+
+    <?php
+        include ('head.php');
+    ?>
+
+    <main>
+        <section>
+            <div class="intro">
+            <h2>Sobre mim</h2>
+            
+            <p>Me chamo Mateus e sou um profissional de TI apaixonado por tecnologia e soluções práticas. Como estudante de <strong>Análise e Desenvolvimento de Sistemas</strong>, combino conhecimento acadêmico com experiência prática adquirida como auxiliar de informática no Exército Brasileiro.</p>
+
+            <p>Complementei minha formação com cursos especializados pelo SENAI em áreas como manutenção de computadores, redes, web design e banco de dados, além de certificações em Desenvolvimento Web e Suporte Técnico. Acredito que essa combinação de conhecimentos técnicos e experiência prática me permite abordar os desafios de TI com uma perspectiva abrangente e soluções eficientes.</p>
+
+            </div>
+    </main>
+
+    <script src="script.js"></script>
+    <footer>
+        <p>Atenciosamente,</p>
+        <p>Mateus Gonçalves Batista da Silva</p>
+        <p>Técnico de Informática | Estudante de ADS</p>
+    </footer>
+</body>
+</html>
